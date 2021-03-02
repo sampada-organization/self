3d10bb1e4d77b25c3b051727ac8255f09b79ecac
Sampada Self <sampada.organization@gmail.com>
2021-03-03 00:27:02 +0530

    config.yml hotfix-2

--- .eleventy.js
+++ .eleventy.js
@@ -1,5 +1,6 @@
 module.exports = function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy("assets");
+  eleventyConfig.addPassthroughCopy("src/admin/config.yml");
   return {
     passthroughFileCopy: true,
     mardownTemplateEngine: "njk",
